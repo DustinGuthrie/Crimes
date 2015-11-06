aed5df2d8b83ee131392a4a26d472ab62cceda44
Agronis <agronis@icloud.com>
2015-11-06 10:21:24 -0500

    Initial Import.1

---
+++ backEnd/src/com/theironyard/Crime.java
@@ -0,0 +1,33 @@
+package com.theironyard;
+/**
+ * Created by Agronis on 11/5/15.
+ */
+public class Crime {
+
+    String name;
+    String abbrev;
+    int year;
+    int population;
+    int total;
+    int murder;
+    int manslaughter;
+    int rape;
+    int robbery;
+    int assault;
+
+    public Crime() {}
+
+    public Crime(String name, String abbrev, int year, int population, int total, int murder, int manslaughter, int rape, int robbery, int assault) {
+
+        this.name = name;
+        this.abbrev = abbrev;
+        this.year = year;
+        this.population = population;
+        this.total = total;
+        this.murder = murder;
+        this.manslaughter = manslaughter;
+        this.rape = rape;
+        this.robbery = robbery;
+        this.assault = assault;
+    }
+}
